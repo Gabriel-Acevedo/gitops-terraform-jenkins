2bbbcaba7c2b23791169230d56db5bba14cd2ef9
Gabriel-Acevedo <55878061+Gabriel-Acevedo@users.noreply.github.com>
2020-06-14 12:42:17 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
-    key    = "terraform.tfstate"
+    bucket = "terraform-bucket-gga"
+    key    = "terraform-bucket-gga"
     region = "us-east-1"
   }
 }
