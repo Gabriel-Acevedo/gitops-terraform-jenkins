a2439d3318de4b42839e9ce25bd2de46edca2ede
Gabriel-Acevedo <55878061+Gabriel-Acevedo@users.noreply.github.com>
2020-06-15 19:49:29 +0200

    adding_port22
    adding_port22

--- main.tf
+++ main.tf
@@ -28,13 +28,20 @@ resource "aws_instance" "default" {
 
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
+  name = "terraform-22-80-sg"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    }
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
 }
